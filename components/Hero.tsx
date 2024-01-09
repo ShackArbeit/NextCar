fd9a8906459387e93d07b418b8c1381728ea291a
Shack Lin <g0972222165@gmail.com>
2024-01-09 09:19:52 +0800

    Add Footer And Navbar

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,41 +1,45 @@
-'use client'
-import Image from "next/image"
-import { CustomButton } from "."
+"use client";
+
+import Image from "next/image";
+import { CustomButton } from ".";
+
 
 const Hero = () => {
-    const handleScroll=()=>{
+  const handleScroll = () => {
+    // const nextSection = document.getElementById("discover");
 
-    }
+    // if (nextSection) {
+    //   nextSection.scrollIntoView({ behavior: "smooth" });
+    // }
+  };
+
   return (
-    <div className='hero'>
-        <div className='flex-1 pt-36 padding-x'>
-            <h1 className='hero__title'>
-                Find Book or Rent a Car Quickly 
-                And Easily !
-            </h1>
-            <p className='hero__subtitle'>
-                Streamline Your Car Rental Experience 
-                With Our Effortless Booking Process !
-            </p>
-            <CustomButton
-            title='Explore Cars'
-            containerStyles='bg-primary-blue text-white rounded-full mt-10'
-            handleClick={handleScroll}
-            />
-            <div className='hero__image-container'>
-                <div className='hero__image'>
-                    <Image 
-                    src='/hero.png'
-                    alt='hero'
-                    fill
-                    className='object-container'
-                    />
-                    <div className='hero__image-overlay'/>
-                </div>
-            </div>
-        </div>
-    </div>
-  )
-}
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Find, book, rent a car—quick and super easy!
+        </h1>
 
-export default Hero
+        <p className="hero__subtitle">
+          Streamline your car rental experience with our effortless booking
+          process.
+        </p>
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+          btnType="button"
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+        </div>
+
+        <div className="hero__image-overlay" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
